46d90dcdaada5be3ca6c2e29a3a875a7d62f9847
Martin <escorpmartin97@gmail.com>
2022-06-21 16:07:40 -0300

    feat: consulta com apollo

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,22 @@
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id,
+      title,
+    }
+  }
+`
 
 function App() {
-  
+  useEffect(() => {
+    client.query({
+      query: GET_LESSONS_QUERY,
+    }).then((response) => console.log(response.data));
+  }, [])
 
   return (
     <h1 className="text-2xl">Hello</h1>
