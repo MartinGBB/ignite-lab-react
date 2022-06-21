cd7b71e79bae8bec7875bd29e206e7611845ceb1
Martin <escorpmartin97@gmail.com>
2022-06-21 16:30:27 -0300

    feat: lista lessons e usa insterface para usar formatar lista

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -11,11 +11,24 @@ const GET_LESSONS_QUERY = gql`
   }
 `
 
+interface Lesson {
+  id: string;
+  title: string;
+};
+
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
   console.log(data)
   return (
-    <h1 className="text-2xl">Hello</h1>
+    <ul>
+      {
+        data?.lessons.map((lesson) => {
+          return (
+            <li key={ lesson.id }>{ lesson.title }</li>
+          )
+        })
+      }
+    </ul>
   )
 }
 
