d9774a0e0e138b12343166de459d950eaf4c129e
Martin <escorpmartin97@gmail.com>
2022-06-21 21:40:48 -0300

    feat: cria preview do slidebar

---
+++ vite-project/src/components/Leasson.tsx
@@ -0,0 +1,15 @@
+export function Lesson() {
+  return (
+    <div>
+      <a href="#" />
+      <span>Segunda • 21 de junho • 19h00</span>
+      <div>
+        <header>
+          <span>Conteúdo liberado</span>
+          <span>AO VIVO</span>
+        </header>
+        <strong>Abertura do evento Ignite labs</strong>
+      </div>
+    </div>
+  )
+}
