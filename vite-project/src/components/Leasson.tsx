82e3b411f7655dc97a729ef41e7467f4a43e5fc4
Martin <escorpmartin97@gmail.com>
2022-06-21 21:43:17 -0300

    fix: identa tags e coloca tudo dentro da ancora

--- vite-project/src/components/Leasson.tsx
+++ vite-project/src/components/Leasson.tsx
@@ -1,15 +1,24 @@
 export function Lesson() {
   return (
-    <div>
-      <a href="#" />
-      <span>Segunda • 21 de junho • 19h00</span>
+    <a href="#">
+      <span>
+        Segunda • 21 de junho • 19h00
+      </span>
+
       <div>
         <header>
-          <span>Conteúdo liberado</span>
-          <span>AO VIVO</span>
+          <span>
+            Conteúdo liberado
+          </span>
+          <span>
+            AO VIVO
+          </span>
         </header>
-        <strong>Abertura do evento Ignite labs</strong>
+
+        <strong>
+          Abertura do evento Ignite labs
+        </strong>
       </div>
-    </div>
+    </a>
   )
 }
