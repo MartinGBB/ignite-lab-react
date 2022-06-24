132b8a0ad8b41e22ef1bbc990095592a5ae2b0ab
Martin <escorpmartin97@gmail.com>
2022-06-23 21:13:46 -0300

    feat: traz parametro da rota

--- vite-project/src/pages/Event.tsx
+++ vite-project/src/pages/Event.tsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const { slog } = useParams<{ slug: string }>()
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
