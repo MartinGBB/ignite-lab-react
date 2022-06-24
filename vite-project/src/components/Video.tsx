e5daa1c8c673beef42cce36a6aa8e390b66fe84d
Martin <escorpmartin97@gmail.com>
2022-06-23 21:53:39 -0300

    feat: nova requisicao passando variable

--- vite-project/src/components/Video.tsx
+++ vite-project/src/components/Video.tsx
@@ -1,8 +1,49 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+  lesson(where: {slug: $slug}) {
+    title
+    description
+    videoId
+    teacher {
+      name
+      bio
+      avatarURL
+    }
+  }
+}
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    description: string;
+    videoId: string;
+    teacher: {
+      name: string;
+      bio: string;
+      avatarURL: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    }
+  });
+
+  console.log(data)
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
--- vite-project/src/pages/Event.tsx
+++ vite-project/src/pages/Event.tsx
@@ -4,13 +4,16 @@ import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
-  const { slug } = useParams<{ slug: string }>()
+  const { slug } = useParams<{ slug: string }>();
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        { slug ? <Video /> : <div className="flex-1"></div>}
+        { slug 
+          ? <Video lessonSlug={ slug } /> 
+          : <div className="flex-1"></div> 
+        }
         <Sidebar />
       </main>
     </div>
