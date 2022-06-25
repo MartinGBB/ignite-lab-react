ad68ac56eed5c4e36a47db7a52e20649f8e9bd6e
Martin <escorpmartin97@gmail.com>
2022-06-25 14:07:41 -0300

    feat: configura variaveis de ambiente

--- vite-project/src/lib/apollo.ts
+++ vite-project/src/lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ogw4mq05pu01xsce7he6l0/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
