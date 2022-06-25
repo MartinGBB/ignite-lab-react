abb5e0ff07052e032c1d4015c3dd27316c748d9c
Martin <escorpmartin97@gmail.com>
2022-06-25 14:32:12 -0300

    feat: configura formulario para registrar dados do input

--- vite-project/src/pages/Subscribe.tsx
+++ vite-project/src/pages/Subscribe.tsx
@@ -1,6 +1,15 @@
+import { FormEvent, useState } from "react";
 import { Logo } from "../components/Logo";
 
 export function Subscribe() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleSubscribe(event: FormEvent) {
+    event.preventDefault()
+    console.log(name, email)
+  }
+
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -16,16 +25,18 @@ export function Subscribe() {
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
           <strong className="text-2xl mb-6 block">Insceva-se gratuitamente</strong>
 
-          <form action="" className="flex flex-col gap-2 w-full">
+          <form onSubmit={ handleSubscribe } className="flex flex-col gap-2 w-full">
             <input
               type="text"
               placeholder="Seu nome completo"
-              className="bg-gray-900 rounded px-5 h-14" 
+              className="bg-gray-900 rounded px-5 h-14"
+              onChange={ (event) => setName(event.target.value) }
             />
             <input
               type="email"
               placeholder="Digite seu e-mail"
-              className="bg-gray-900 rounded px-5 h-14" 
+              className="bg-gray-900 rounded px-5 h-14"
+              onChange={ (event) => setEmail(event.target.value) }
             />
             <button
               type="submit"
